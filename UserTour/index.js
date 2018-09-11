a1b180089b41d24268de87d034b3f3cfd2db9514
bogdan_scher <bsc@piogroup.com>
2018-09-11 16:35:26 +0300

    UserTour plugin

---
+++ UserTour/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Joyride from 'react-joyride';
+
+import { tourSteps, toursStyles, locale } from './TourSteps';
+
+const UserTour = ({ run }) => {
+  return (
+    <Joyride
+      continuous
+      hideBackButton={true}
+      showSkipButton={true}
+      showProgress={true}
+      locale={locale}
+      steps={tourSteps}
+      run={run}
+      styles={toursStyles}
+      />
+    );
+};
+
+UserTour.propTypes = {
+  run: PropTypes.boolean,
+};
+
+UserTour.defaultProps = {
+  run: false,
+};
+
+export default UserTour;
