b082247e8e087c436e268c731353aae045450750
bogdan_scher <bsc@piogroup.com>
2018-09-13 17:34:52 +0300

    fixes for Tour

--- UserTour/index.js
+++ UserTour/index.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Joyride from 'react-joyride';
 
 import { tourSteps, toursStyles, locale } from './TourSteps';
 
-const UserTour = ({ run }) => {
-  return (
-    <Joyride
-      continuous
-      hideBackButton={true}
-      showSkipButton={true}
-      showProgress={true}
-      locale={locale}
-      steps={tourSteps}
-      run={run}
-      styles={toursStyles}
-      />
-    );
+export default class UserTour extends Component {
+
+    static propTypes = {
+        run: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        run: false,
+    };
+
+    /**
+     * This function add info to Cookie that user tour was passed
+     *
+     * @param tour
+     */
+    callback = (tour) => {
+      const { type } = tour;
+      if (type === 'tour:end') {
+        document.cookie = 'userTour=passed';
+      }
+    };
+
+    /**
+     * This function check that UserTour was passed
+     *
+     * @return {boolean}
+     */
+    checkIsPassed = () => {
+        const decodedCookie = decodeURIComponent(document.cookie).split(';');
+        return (-1 === decodedCookie.indexOf(' userTour=passed'));
+    };
+
+    render() {
+        const { run } = this.props;
+        const isPassed = (this.checkIsPassed() || run);
+        return (
+            <Joyride
+                continuous
+                disableOverlayClose={true}
+                showSkipButton={true}
+                showProgress={true}
+                locale={locale}
+                steps={tourSteps}
+                run={isPassed}
+                styles={toursStyles}
+                callback={this.callback}
+            />
+        );
+    }
 };
 
-UserTour.propTypes = {
-  run: PropTypes.bool,
-};
 
-UserTour.defaultProps = {
-  run: false,
-};
-
-export default UserTour;
