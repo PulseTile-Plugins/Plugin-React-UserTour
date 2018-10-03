9986cbca247da7fa2006c9b98f9ec5d8ca604ab3
bogdan_scher <bsc@piogroup.com>
2018-10-03 11:30:55 +0300

    New plugins version

--- UserTour/index.js
+++ UserTour/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Joyride from 'react-joyride';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+
+import { clientUrls } from '../../../../../config/client-urls.constants';
+import UserPanelItem from '../../../../containers/UserPanel/UserPanelItem';
+import PTButton from '../../../../ui-elements/PTButton/PTButton';
 
 import { tourSteps, toursStyles, locale } from './TourSteps';
 
@@ -14,6 +20,11 @@ export default class UserTour extends Component {
         run: false,
     };
 
+    state = {
+        isTourRun: false,
+        isTourMode: true,
+    };
+
     /**
      * This function add info to Cookie that user tour was passed
      *
@@ -23,9 +34,28 @@ export default class UserTour extends Component {
       const { type } = tour;
       if (type === 'tour:end') {
         document.cookie = 'userTour=passed';
-        document.getElementById('icon-tour').style.display = 'none';
-        document.getElementById('icon-link').style.display = 'block';
+        this.setState({
+            isTourMode: !this.state.isTourMode,
+        })
       }
+    };
+
+    /**
+     * This function runs User Tour
+     */
+    runTour = () => {
+        this.setState({
+            isTourRun: !this.state.isTourRun,
+        });
+    };
+
+    /**
+     * This function toggles User Tour mode
+     */
+    toggleMode = () => {
+        this.setState({
+            isTourMode: true,
+        })
     };
 
     /**
@@ -39,22 +69,65 @@ export default class UserTour extends Component {
     };
 
     render() {
-        const { run } = this.props;
-        const isPassed = (this.checkIsPassed() || run);
-        return (
-            <Joyride
-                continuous
-                disableOverlayClose={true}
-                showSkipButton={true}
-                showProgress={true}
-                locale={locale}
-                steps={tourSteps}
-                run={isPassed}
-                styles={toursStyles}
-                callback={this.callback}
-            />
-        );
+        const { router } = this.props;
+        const { isTourRun, isTourMode } = this.state;
+
+        const routerHash = (router.location.hash.split('?')[0]).split('#')[1];
+        const routerHashArray = routerHash.split('/');
+        const userId = get(routerHashArray, '[2]', null);
+        const pageUrl = routerHashArray.pop();
+        const homepageLink = `${clientUrls.PATIENTS}/${userId}/${clientUrls.PATIENTS_SUMMARY}`;
+
+        const isPassed = (this.checkIsPassed() || isTourRun);
+
+            return (
+                <UserPanelItem className="user-panel-item">
+                    {(clientUrls.PATIENTS_SUMMARY === pageUrl)
+                        ?
+                        (isTourMode ? <UserTourButton runTour={this.runTour} /> : <LinkToCustomer />)
+                        :
+                        <Link to={homepageLink} onClick={this.toggleMode}>
+                            <PTButton title="Home" className="btn-header">
+                                <i className="fa fa-question-circle" />
+                            </PTButton>
+                        </Link>
+                    }
+                    <Joyride
+                        continuous
+                        disableOverlayClose={true}
+                        showSkipButton={true}
+                        showProgress={true}
+                        locale={locale}
+                        steps={tourSteps}
+                        run={isPassed}
+                        styles={toursStyles}
+                        callback={this.callback}
+                    />
+                </UserPanelItem>
+            );
     }
 };
 
+const UserTourButton = ({ runTour }) => {
+    return (
+        <PTButton title="User Tour" id="icon-tour" className="btn-header" onClick={() => runTour()}>
+            <i className="fa fa-question-circle" />
+        </PTButton>
+    )
+};
+UserTourButton.propTypes = {
+    runTour: PropTypes.func,
+};
+UserTourButton.defaultProps = {
+    runTour: function () { },
+};
 
+const LinkToCustomer = () => {
+    return (
+        <a href="http://myhelm.org" target="_blank">
+            <PTButton title="MyHelm.org" id="icon-link" className="btn-header">
+                <i className="fa fa-question-circle" />
+            </PTButton>
+        </a>
+    )
+};
