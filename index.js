a30ca59eac0e2f3c8b86594f80a152b06651e13f
bogdan_scher <bsc@piogroup.com>
2018-10-09 12:07:29 +0300

    remove pageURL definition

--- index.js
+++ index.js
@@ -14,120 +14,111 @@ import './style.scss';
 
 export default class UserTour extends Component {
 
-    state = {
-        isTourRun: false,
-        isTourMode: true,
-    };
+  state = {
+    isTourRun: false,
+    isTourMode: true,
+  };
 
-    /**
-     * This function add info to Cookie that user tour was passed
-     *
-     * @param tour
-     */
-    callback = (tour) => {
-      const { type } = tour;
-      if (type === 'tour:end') {
-        document.cookie = 'userTour=passed';
-        this.setState({
-            isTourMode: !this.state.isTourMode,
-        })
-      }
-    };
-
-    /**
-     * This function runs User Tour
-     */
-    runTour = () => {
-        this.setState({
-            isTourRun: !this.state.isTourRun,
-        });
-    };
-
-    /**
-     * This function toggles User Tour mode
-     */
-    toggleMode = () => {
-        this.setState({
-            isTourMode: true,
-        })
-    };
-
-    /**
-     * This function check that UserTour was passed
-     *
-     * @return {boolean}
-     */
-    checkIsPassed = () => {
-        const decodedCookie = decodeURIComponent(document.cookie).split(';');
-        return !(-1 !== decodedCookie.indexOf('userTour=passed') || -1 !== decodedCookie.indexOf(' userTour=passed'));
-    };
-
-    render() {
-        const { router } = this.props;
-        const { isTourRun, isTourMode } = this.state;
-
-        const routerHash = (router.location.hash.split('?')[0]).split('#')[1];
-        const routerHashArray = routerHash.split('/');
-        const userId = get(routerHashArray, '[2]', null);
-        const pageUrl = routerHashArray.pop();
-        const homepageLink = `${clientUrls.PATIENTS}/${userId}/${clientUrls.PATIENTS_SUMMARY}`;
-
-        const isPassed = (this.checkIsPassed() || isTourRun);
-
-            return (
-                <UserPanelItem className="user-panel-item">
-                    {(clientUrls.PATIENTS_SUMMARY === pageUrl)
-                        ?
-                        (isTourMode ?
-                            <div>
-                                <UserTourButton runTour={this.runTour} />
-                                <Joyride
-                                    continuous
-                                    disableOverlayClose={true}
-                                    showSkipButton={true}
-                                    showProgress={true}
-                                    locale={locale}
-                                    steps={tourSteps}
-                                    run={isPassed}
-                                    styles={toursStyles}
-                                    callback={this.callback}
-                                />
-                            </div>
-                            :
-                            <LinkToCustomer />)
-                        :
-                        <Link to={homepageLink} onClick={this.toggleMode}>
-                            <PTButton title="Home" className="btn-header">
-                                <i className="fa fa-question-circle" />
-                            </PTButton>
-                        </Link>
-                    }
-                </UserPanelItem>
-            );
+  /**
+   * This function add info to Cookie that user tour was passed
+   *
+   * @param tour
+   */
+  callback = (tour) => {
+    const { type } = tour;
+    if (type === 'tour:end') {
+      document.cookie = 'userTour=passed';
+      this.setState({
+        isTourMode: !this.state.isTourMode,
+      })
     }
+  };
+
+  /**
+   * This function runs User Tour
+   */
+  runTour = () => {
+    this.setState({
+      isTourRun: !this.state.isTourRun,
+    });
+  };
+
+  /**
+   * This function toggles User Tour mode
+   */
+  toggleMode = () => {
+    this.setState({
+      isTourMode: true,
+    })
+  };
+
+  /**
+   * This function check that UserTour was passed
+   *
+   * @return {boolean}
+   */
+  checkIsPassed = () => {
+    const decodedCookie = decodeURIComponent(document.cookie).split(';');
+    return !(-1 !== decodedCookie.indexOf('userTour=passed') || -1 !== decodedCookie.indexOf(' userTour=passed'));
+  };
+
+  render() {
+    const { pageUrl, homepageLink } = this.props;
+    const { isTourRun, isTourMode } = this.state;
+    const isPassed = (this.checkIsPassed() || isTourRun);
+    return (
+      <UserPanelItem className="user-panel-item">
+        {(clientUrls.PATIENTS_SUMMARY === pageUrl)
+          ?
+          (isTourMode ?
+            <div>
+              <UserTourButton runTour={this.runTour} />
+              <Joyride
+                continuous
+                disableOverlayClose={true}
+                showSkipButton={true}
+                showProgress={true}
+                locale={locale}
+                steps={tourSteps}
+                run={isPassed}
+                styles={toursStyles}
+                callback={this.callback}
+              />
+            </div>
+              :
+            <LinkToCustomer />)
+            :
+          <Link to={homepageLink} onClick={this.toggleMode}>
+            <PTButton title="Home" className="btn-header">
+              <i className="fa fa-question-circle" />
+            </PTButton>
+          </Link>
+        }
+      </UserPanelItem>
+    );
+  }
 };
 
 const UserTourButton = ({ runTour }) => {
-    return (
-        <PTButton title="User Tour" id="icon-tour" className="btn-header" onClick={() => runTour()}>
-            <i className="fa fa-question-circle" />
-
-        </PTButton>
-    )
+  return (
+    <PTButton title="User Tour" id="icon-tour" className="btn-header" onClick={() => runTour()}>
+      <i className="fa fa-question-circle" />
+    </PTButton>
+  );
 };
 UserTourButton.propTypes = {
-    runTour: PropTypes.func,
+  runTour: PropTypes.func,
 };
 UserTourButton.defaultProps = {
-    runTour: function () { },
+  runTour: function () { },
 };
 
 const LinkToCustomer = () => {
-    return (
-        <a href="http://myhelm.org" target="_blank">
-            <PTButton title="MyHelm.org" id="icon-link" className="btn-header">
-                <i className="fa fa-question-circle" />
-            </PTButton>
-        </a>
-    )
+  return (
+    <a href="http://myhelm.org" target="_blank">
+      <PTButton title="MyHelm.org" id="icon-link" className="btn-header">
+        <i className="fa fa-question-circle" />
+      </PTButton>
+    </a>
+  );
 };
